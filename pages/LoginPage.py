efbb3eeede3b0888fdf312c855dfaccf812c5042
Dagtret <sergeyaggo2015@gmail.com>
2025-06-02 08:36:57 +0300

    Добавил алюр отчеты в тесты + добавил в gitignore папку allure-results

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,3 +1,5 @@
+import allure
+
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
@@ -33,14 +35,22 @@ class LoginPageHelper(BasePage):
         self.find_element(LoginPageLocators.MAIL_LOGIN_BUTTON)
         self.find_element(LoginPageLocators.YANDEX_LOGIN_BUTTON)
 
+    @allure.step('Нажимаем на кнопку ВОЙТИ')
     def click_login_button(self):
+        self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
 
+    @allure.step('Получаем текст ошибки')
     def get_error_text(self):
+        self.attach_screenshot()
         return self.find_element(LoginPageLocators.ERROR_TEXT).text
 
+    @allure.step('Нажимаем на поле ЛОГИН')
     def click_login_field(self):
+        self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_FIELD).click()
 
+    @allure.step('Вводим логин')
     def input_login(self, value):
+        self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys(value)
